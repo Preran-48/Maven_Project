9cc2a5ac3b4a0a67de783ff007c499071a0f7cd5
Preran-48 <prerankumar.n@qspiders.in>
2026-02-06 12:49:53 +0530

    Add message for configured Poll SCM

--- src/main/java/com/google/App.java
+++ src/main/java/com/google/App.java
@@ -6,5 +6,6 @@ package com.google;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins");
+        System.out.println("Configured Poll SCM");
     }
 }
